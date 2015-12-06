e8f0ac70fda75ae54585609524ca78e5f6d36b94
Alex Schwartz <alexschwartz@users.noreply.github.com>
2015-12-06 23:28:12 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,19 @@
-echo -e "\e[1mbold\e[0m"
-echo -e "\e[3mitalic\e[0m"
-echo -e "\e[4munderline\e[0m"
-echo -e "\e[9mstrikethrough\e[0m"
-echo -e "\e[31mHello World\e[0m"
-echo -e "\x1B[31mHello World\e[0m"
+
+a=0
+
+while [ $a -lt 100 ]
+do
+   echo $a
+   a=`expr $a + 1`
+   echo -e "\e[1mbold\e[0m"
+   echo -e "\e[3mitalic\e[0m"
+   echo -e "\e[4munderline\e[0m"
+   echo -e "\e[9mstrikethrough\e[0m"
+   echo -e "\e[31mHello World\e[0m"
+   echo -e "\x1B[31mHello World\e[0m"
+   sleep 1
+done
+
+
+
+
